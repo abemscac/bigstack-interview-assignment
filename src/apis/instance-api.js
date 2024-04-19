932b04072c3cab79b9089b8976b6b386f376330e
abemscac <abe.msc.ac@gmail.com>
2024-04-20 07:29:30 +0800

    fix: not loading tags from localStorage bug

--- src/apis/instance-api.js
+++ src/apis/instance-api.js
@@ -1,7 +1,19 @@
 import useSWR from 'swr';
+import { getStoredTags } from '@utilities/instance-util';
 
 const useListInstances = () => {
-  return useSWR('listInstances', () => require('@server-data/instances.json'));
+  return useSWR('listInstances', () => {
+    const instances = require('@server-data/instances.json');
+
+    instances.forEach(instance => {
+      const storedTags = getStoredTags(instance.id);
+      if (storedTags) {
+        instance.tags = storedTags;
+      }
+    });
+
+    return instances;
+  });
 };
 
 export const InstanceApi = {
