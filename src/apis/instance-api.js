29c4d364c57fc2c48b9d6fa2c0edbe362e02efb5
abemscac <abe.msc.ac@gmail.com>
2024-04-20 07:23:26 +0800

    chore: remove gh-pages deploy from main

--- src/apis/instance-api.js
+++ src/apis/instance-api.js
@@ -3,29 +3,20 @@ import { getStoredTags } from '@utilities/instance-util';
 import { axios } from './axios-instance';
 
 const useListInstances = () => {
-  let fetcher = undefined;
+  return useSWR('listInstances', async () => {
+    const { data } = await axios.get('/instances');
 
-  if (process.env.NODE_ENV === 'production') {
-    // Use fixed instances data in production environment. (for Github Pages)
-    fetcher = () => require('@server-data/instances.json');
-  } else {
-    // Get instances from server in non-production environment.
-    fetcher = async () => {
-      const { data } = await axios.get('/instances');
+    if (Array.isArray(data)) {
+      data.forEach(instance => {
+        const storedTags = getStoredTags(instance.id);
+        if (storedTags) {
+          instance.tags = storedTags;
+        }
+      });
+    }
 
-      if (Array.isArray(data)) {
-        data.forEach(instance => {
-          const storedTags = getStoredTags(instance.id);
-          if (storedTags) {
-            instance.tags = storedTags;
-          }
-        });
-      }
-      return data;
-    };
-  }
-
-  return useSWR('listInstances', fetcher);
+    return data;
+  });
 };
 
 export const InstanceApi = {
